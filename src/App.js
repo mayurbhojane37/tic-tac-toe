bb6e33b4f579d9324745a06399ca437995afc52f
mayurbhojane37 <mayurbhojane37@gmail.com>
2024-03-27 13:59:43 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import TicTacToe from './Components/TicTacToe/TicTacToe';
+
+
+function App() {
+  return (
+    <div >
+      <TicTacToe></TicTacToe>
+    </div>
+  );
+}
+
+export default App;
